4b767874f9369aa611fc35489de13490ab602ce7
jaisheel41 <jaisheel42@gmail.com>
2025-01-09 17:43:31 +0000

    api changes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/', // Base URL for the Django backend
+    baseURL: 'https://jaisheel42.pythonanywhere.com/api/', // Base URL for the Django backend
     headers: {
         'Content-Type': 'application/json',
     },
